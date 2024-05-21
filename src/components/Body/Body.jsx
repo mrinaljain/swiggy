501fda3b4b3ea68acd572a46a46783bef723a8b1
mrinaljain <jain.mrinal140@gmail.com>
2024-05-21 20:14:41 +0530

    filters added

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -4,12 +4,26 @@ import RestaurentCard from "../RestaurentCard/RestaurentCard";
 import restaurants from "../../utils/mockData";
 
 const Body = function () {
+   const [restaurantList, setRestaurentList] = React.useState(restaurants);
+   function filter(rating) {
+      setRestaurentList((oldList) => {
+         if (rating === 0) {
+            return restaurants;
+         }
+         let list = restaurantList.filter((oldRestaurent) => oldRestaurent.info.avgRating >= rating);
+         return list;
+      });
+   }
    return (
       <div>
          <h3>Restaurants with online food delivery in Indore</h3>
+         <button onClick={() => filter(4.5)}> Top Rated </button>
+         <button onClick={() => filter(4.4)}> 4.4 </button>
+         <button onClick={() => filter(4.3)}> 4.3 </button>
+         <button onClick={() => filter(4.2)}> 4.2 </button>
+         <button onClick={() => filter(0)}> CLEAR </button>
          <div className="foodContainer">
-            {restaurants.map((restaurant) => <RestaurentCard key={restaurant.id} restaurantData={restaurant} />)}
-
+            {restaurantList.map((restaurant) => <RestaurentCard key={restaurant.info.id} restaurantData={restaurant} />)}
          </div>
       </div>
    );
--- src/components/RestaurentCard/RestaurentCard.jsx
+++ src/components/RestaurentCard/RestaurentCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./RestaurentCard.css";
 const RestaurentCard = (props) => {
-   const { name, areaName, costForTwo, cloudinaryImageId } = props.restaurantData.info;
+   const { name, areaName, costForTwo, cloudinaryImageId, avgRating } = props.restaurantData.info;
    function handelClick(e) {
       e.preventDefault();
       window.location = props.restaurantData.cta.link;
@@ -9,10 +9,11 @@ const RestaurentCard = (props) => {
    }
    return (
       <div className="restaurentCard">
-         <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`} alt="{name}" className="restaurentImage" />
+         <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`} alt={name} className="restaurentImage" />
          <h3 className="restaurentName">{name}</h3>
          <p className="restaurentDescription">{areaName}</p>
          <p className="restaurentPrice">{costForTwo}</p>
+         <p className="restaurentPrice">{avgRating}</p>
          <button className="orderButton" onClick={handelClick}>Order Now</button>
       </div>
    )
