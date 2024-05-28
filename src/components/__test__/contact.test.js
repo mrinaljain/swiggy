2623e7b0db02bb2e76051e0bd661da04cfa18d39
mrinaljain <jain.mrinal140@gmail.com>
2024-05-28 15:40:43 +0530

    removed coverage

--- src/components/__test__/contact.test.js
+++ src/components/__test__/contact.test.js
@@ -2,11 +2,24 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../../pages/Contact/Contact";
 import "@testing-library/jest-dom";
 
-test("check if contact screen loaded", () => {
-  render(<Contact />);
-  //Querrying
-  const heading = screen.getByRole("heading"); // returns a js object for the element
 
-  //assertion
-  expect(heading).toBeInTheDocument();
+describe("Contact Us screen", () => {
+  test("check if contact screen loaded", () => {
+    render(<Contact />);
+    //Querrying
+    const heading = screen.getByRole("heading"); // returns a js object for the element
+    //assertion
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Check if input is loaded", () => {
+    //render screen
+    render(<Contact />);
+    //find element
+    const inputs = screen.getAllByRole("textbox");
+    //compare
+
+    expect(inputs.length).toBe(3);
+  });
 });
+
