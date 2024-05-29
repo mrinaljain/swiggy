68cbf4889ec46d204fa324b1e9220e894bcf2a4f
mrinaljain <jain.mrinal140@gmail.com>
2024-05-29 14:51:57 +0530

    initial lazy load done Post API not working 403

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 // import "./Body.css"
 import RestaurantCard, { withPromotedLable } from "./RestaurantCard/RestaurantCard";
 import Carousel from "./Carousal/Carousal";
 import useRestaurantList from "../utils/useRestaurantList";
 import Shimmer from "./Shimmer/Shimmer";
+import useScrollDetector from "../utils/useScrollDetector";
+import useUpdatedResList from "../utils/useUpdatedResList";
 
 const Body = function () {
-   let restaurantData = useRestaurantList();
-
+   const restaurantData = useRestaurantList();
+   const scrollDectot = useScrollDetector();
    const [searchKeyword, setsearchKeyword] = useState("");
    const [filter, setFilter] = useState(false);
+   let apiCallHappening = false;
+   // if (scrollDectot && !apiCallHappening) {
+   // apiCallHappening = true;
+
+   let updatedRes = useUpdatedResList(scrollDectot);
+   // apiCallHappening = false;
+
+   // make post apicall
+   // call it whenever 200px scroll is left
+   // make api call
+   // append the result to the existing Restaurant list
+   // }
    function handleFilter() {
       setFilter(true);
    }
@@ -25,6 +39,7 @@ const Body = function () {
    } else {
       const { cards } = restaurantData.data;
       let restaurantList = cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      restaurantList = [...restaurantList, ...updatedRes];
       let topRestaurantList = cards[0].card.card.imageGridCards.info;
 
       let restaurantCardsList = filter ? restaurantList.filter((oldRestaurent) => oldRestaurent.info.avgRating >= 4.5) : searchKeyword.length > 0
@@ -35,11 +50,13 @@ const Body = function () {
          : restaurantList;
       const UpdatedCard = withPromotedLable(RestaurantCard);
       return (
-         <div className="body w-5/6  mx-auto">
-
+         <div
+            className="body w-5/6  mx-auto">
             {topRestaurantList.length && <Carousel restaurantList={topRestaurantList} />}
-            <div className="flex items-center">
-               <div className="my-4 mr-4 py-4 ">
+            <div
+               className="flex items-center">
+               <div
+                  className="my-4 mr-4 py-4 ">
                   <input
                      className="border border-solid border-black w-80 py-2 px-4 rounded-lg"
                      type="text"
@@ -53,20 +70,22 @@ const Body = function () {
                   className="py-2 px-4 bg-gray-100 rounded-lg"
                   onClick={handleFilter}
                   name="filter"
-               >Top Rated</button>
+               >Top Rated
+               </button>
                <button
                   className="py-2 px-4 mx-2 bg-red-100 rounded-lg"
                   onClick={clearFilter}
                   name="clear"
-               > CLEAR </button>
+               > CLEAR
+               </button>
             </div>
             <h3 className="font-bold">Restaurants with online food delivery in Indore</h3>
             <div className="flex flex-wrap justify-center gap-3">
-               {restaurantCardsList.map((restaurant) => {
+               {restaurantCardsList.map((restaurant, index) => {
                   return restaurant?.info?.veg ?
-                     <UpdatedCard key={restaurant.info.id} restaurantData={restaurant} />
+                     <UpdatedCard key={index} restaurantData={restaurant} />
                      :
-                     <RestaurantCard key={restaurant.info.id} restaurantData={restaurant} />
+                     <RestaurantCard key={index} restaurantData={restaurant} />
                })}
             </div>
          </div>
